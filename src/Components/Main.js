4f572da5c2b003caebd6462c1b9abace1fbf7751
sachin <sachinv1986@gmail.com>
2022-03-25 13:20:58 +0530

    initial commit

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -5,13 +5,29 @@ import useMainHooks from "../Hooks/useMainHooks";
 
 const Main = () => {
   const [searchInput, setSearchInput] = useState("");
+  const [nxtState, setNxtState] = useState({ a: 0, b: 4 });
 
   const searchOnChange = (e) => {
     setSearchInput(e.target.value);
   };
 
-  const { filterList, toDoListHere, manageFav, manageDelete } =
-    useMainHooks(searchInput);
+  const { filterList, toDoListHere, manageFav, manageDelete } = useMainHooks(
+    searchInput,
+    nxtState
+  );
+
+  const goNext = () => {
+    const getLength = toDoListHere.length;
+    if (nxtState.b < getLength) {
+      setNxtState({ ...nxtState, a: nxtState.a + 4, b: nxtState.b + 4 });
+    }
+  };
+
+  const goPrevious = () => {
+    if (nxtState.a !== 0) {
+      setNxtState({ ...nxtState, a: nxtState.a - 4, b: nxtState.b - 4 });
+    }
+  };
 
   return (
     <div>
@@ -24,6 +40,8 @@ const Main = () => {
           manageFav={manageFav}
           manageDelete={manageDelete}
         />
+        <button onClick={goPrevious}>Previous</button>
+        <button onClick={goNext}>Next</button>
       </HeaderApp>
     </div>
   );
--- src/Hooks/useMainHooks.js
+++ src/Hooks/useMainHooks.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-const useMainHooks = (searchInput) => {
+const useMainHooks = (searchInput, nxtCount) => {
   const toDoListHere = [
     { name: "FeedDog", favourite: true, id: 1 },
     { name: "Call Parents", favourite: false, id: 2 },
@@ -10,21 +10,31 @@ const useMainHooks = (searchInput) => {
     { name: "React", favourite: false, id: 6 },
     { name: "JavaScript", favourite: false, id: 7 },
     { name: "Unit Testing", favourite: false, id: 8 },
+    { name: "Jio", favourite: true, id: 10 },
+    { name: "Jio Parents", favourite: false, id: 11 },
+    { name: "Jio Life", favourite: true, id: 12 },
+    { name: "karlo ", favourite: true, id: 13 },
+    { name: "dunoya", favourite: false, id: 14 },
+    { name: "muthi", favourite: true, id: 15 },
+    { name: "mai", favourite: false, id: 16 },
+    { name: "mphasis", favourite: false, id: 17 },
+    { name: "Tsc", favourite: false, id: 88 },
   ];
 
   const [filterList, setFilterList] = useState(toDoListHere);
 
   useEffect(() => {
     const makeList = toDoListHere.filter((value, index) => {
-      if (value.name.indexOf(searchInput) !== -1) {
+      if (value.name.toLocaleLowerCase().indexOf(searchInput.toLocaleLowerCase()) !== -1) {
         return value;
       }
     });
     const Fav = makeList.filter((item) => item.favourite);
     const notFav = makeList.filter((item) => !item.favourite);
-    const comBo = [...Fav, ...notFav];
-    setFilterList(comBo);
-  }, [searchInput]);
+    const comBo = [...Fav, ...notFav]; 
+    const FinalRec = comBo.slice(nxtCount.a, nxtCount.b);
+    setFilterList(FinalRec);
+  }, [searchInput, nxtCount]);
 
   const manageFav = (param) => {
     const makeList = toDoListHere.map((value, index) => {
